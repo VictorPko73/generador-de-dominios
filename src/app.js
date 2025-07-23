5eeb1c9072798124561a7d4a94d05781fd50efde
VictorPko73 <152915385+VictorPko73@users.noreply.github.com>
2025-07-23 18:01:05 +0000

    Actualizado el codigo eliminando un arrays que era inecesario

--- src/app.js
+++ src/app.js
@@ -8,24 +8,27 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
-  let pronoun = ['the', 'our'];
-  let adj = ['great', 'big'];
-  let noun = ['jogger', 'racoon'];
-  let extension = ['.com', '.net']
-  let dominios = [];
+  const pronoun = ['the', 'our'];
+  const adj = ['great', 'big'];
+  const noun = ['jogger', 'racoon'];
+  const extension = ['.com', '.net']
+  const result = document.getElementById('dominio')
   
+  let dominios = '';
   pronoun.forEach((pronouns) => {
     adj.forEach((adjs)=>{
       noun.forEach((nouns) => {
         extension.forEach((extensions) =>{
-          dominios.push(`${pronouns}${adjs}${nouns}${extensions}`)
+           dominios += (`${pronouns}${adjs}${nouns}${extensions}<br>`)
+          
         });
         
       });
     });
   });
+  result.innerHTML = dominios
   console.log(dominios);
 
-  document.getElementById('dominio').innerHTML = dominios.join('<br>');
+  
 
 };
